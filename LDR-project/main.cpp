2eb70b632e947ec3047e630f7e274f893d4a1449
lcnMaran <luciano.maran@protonmail.com>
2023-01-02 21:50:54 -0300

    upload

--- LDR-project/main.cpp
+++ LDR-project/main.cpp
@@ -2,14 +2,16 @@
  *                Leitura LDR
  * 
  * @author: Luciano Maran
- * @date:   Dezembro de 2022
+ * @date:   Janeiro de 2023
  * 
 ********************************************************/
 #include <Arduino.h>
 
 
-#define bt_LDR     9
-#define bt_CHUVA  10
+#define bt_LDR      9
+#define bt_CHUVA   10
+#define sensor_LDR  8
+#define myOutput    7
 
 
 uint8_t rtn_Sensor(); //Protótipo de função
@@ -20,32 +22,43 @@ void setup()
   Serial.begin(9600);
 
   //Configuração pinos de entrada
-  pinMode(bt_LDR,   INPUT);
-  pinMode(bt_CHUVA, INPUT);
+  pinMode(bt_LDR,     INPUT);
+  pinMode(bt_CHUVA,   INPUT);
+  pinMode(sensor_LDR, INPUT);
+
+  //Configuração pino de saída
+  pinMode(myOutput,   OUTPUT);
+
 }
+
 
 void loop() 
 {
 
   switch (rtn_Sensor()){
-    case 0:
-    Serial.println("Sensores ativos");
+    case 0: //Sensores ativos
+    digitalRead(sensor_LDR) == 0 ? Serial.println("LDR: Noite") : Serial.println("LDR: Dia");
     break;
     
-    case 1:
-    Serial.println("Sensor CHUVA: ativo");
+    case 1: //Sensor CHUVA: ativo
     break;
 
-    case 2:
-    Serial.println("Sensor LDR: ativo");
+    case 2: //Sensor LDR: ativo
+    if(digitalRead(sensor_LDR)) //Outra maneira de fazer verificação
+    {
+      digitalWrite(myOutput, LOW);
+      Serial.println("LDR: Dia");
+    }else
+    {
+      digitalWrite(myOutput, HIGH);
+      Serial.println("LDR: Noite");
+    }
     break;
 
-    case 3:
-    Serial.println("Sensores desativados");
+    case 3: //Sensores desativados
     break;
   
-    default:
-    //Conselho: Sempre use default!
+    default: //Conselho: Sempre use default!
     break;
   }
 
